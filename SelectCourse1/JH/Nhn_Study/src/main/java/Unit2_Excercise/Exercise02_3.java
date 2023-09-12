5249832aaa3b9e766efd5d5afd3e7c4447ac513b
quswjdgns399 <quswjdgns3996@gmail.com>
2023-09-12 17:23:18 +0900

    first commit

---
+++ SelectCourse1/JH/Nhn_Study/src/main/java/Unit2_Excercise/Exercise02_3.java
@@ -0,0 +1,9 @@
+package Unit2_Excercise;
+
+public class Exercise02_3 {
+
+    public static void main(String[] args) {
+        String name = "junghun";
+        System.out.println(name.substring(0, 1) + name.substring(name.length() - 1));
+    }
+}
---
+++ SelectCourse1/JH/Nhn_Study/src/main/java/Unit2_Excercise/Exercise02_5.java
@@ -0,0 +1,15 @@
+//package Unit2_Excercise;
+//
+//import javax.swing.JOptionPane;
+//
+//public class Exercise02_5 {
+//    public static void main(String[] args) {
+//        String str = JOptionPane.showInputDialog("Insert String");
+//        StringBuffer strBuffer = new StringBuffer(str);
+//
+//        int temp = str.length() - 1;
+//        strBuffer.replace(str.length() - 1, str.length(), 0, 1);
+//
+//
+//    }
+//}
