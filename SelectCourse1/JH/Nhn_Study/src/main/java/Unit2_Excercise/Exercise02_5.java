03f1d1a6030f6f48e929a3fc15af1f97710cb951
quswjdgns399 <quswjdgns3996@gmail.com>
2023-09-12 17:38:30 +0900

    first commit

--- SelectCourse1/JH/Nhn_Study/src/main/java/Unit2_Excercise/Exercise02_5.java
+++ SelectCourse1/JH/Nhn_Study/src/main/java/Unit2_Excercise/Exercise02_5.java
@@ -1,15 +1,16 @@
-//package Unit2_Excercise;
-//
-//import javax.swing.JOptionPane;
-//
-//public class Exercise02_5 {
-//    public static void main(String[] args) {
-//        String str = JOptionPane.showInputDialog("Insert String");
-//        StringBuffer strBuffer = new StringBuffer(str);
-//
-//        int temp = str.length() - 1;
-//        strBuffer.replace(str.length() - 1, str.length(), 0, 1);
-//
-//
-//    }
-//}
+package Unit2_Excercise;
+
+import javax.swing.JOptionPane;
+
+public class Exercise02_5 {
+    public static void main(String[] args) {
+        String str = JOptionPane.showInputDialog("Insert String");
+        StringBuffer strBuffer = new StringBuffer(str);
+
+        String afterstr = strBuffer.substring(strBuffer.length() - 1) + strBuffer.substring(1, strBuffer.length() - 1) +
+                strBuffer.substring(0, 1);
+
+        System.out.println(afterstr);
+
+    }
+}
