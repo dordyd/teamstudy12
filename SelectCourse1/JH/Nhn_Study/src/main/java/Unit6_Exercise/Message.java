a370d0fffee6753b9c0efd3965a1dabc1a6fa0ba
quswjdgns399 <quswjdgns3996@gmail.com>
2023-09-20 15:21:45 +0900

    Unit6 Message modified

--- SelectCourse1/JH/Nhn_Study/src/main/java/Unit6_Exercise/Message.java
+++ SelectCourse1/JH/Nhn_Study/src/main/java/Unit6_Exercise/Message.java
@@ -12,7 +12,6 @@ public class Message {
         this.text = text;
     }
 
-    // Getter and Setter methods for sender, receiver, and text
     public String getSender() {
         return sender;
     }
@@ -37,26 +36,21 @@ public class Message {
         this.text = text;
     }
 
-    // Method to swap sender and receiver
     public void swapSenderReceiver() {
         String temp = sender;
         sender = receiver;
         receiver = temp;
     }
 
-    // Method to compress the message text
     public void compressMessage(int option) {
         switch (option) {
             case 1:
-                // Eliminate leading/trailing whitespace and replace multiple spaces with a single space
                 text = text.trim().replaceAll("\\s+", " ");
                 break;
             case 2:
-                // Eliminate all vowels from the text
                 text = text.replaceAll("[aeiouAEIOU]", "");
                 break;
             case 3:
-                // Perform both (1) and (2)
                 text = text.trim().replaceAll("\\s+", " ").replaceAll("[aeiouAEIOU]", "");
                 break;
             default:
@@ -70,19 +64,18 @@ public class Message {
     }
 
     public static void main(String[] args) {
-        Message message = new Message("Alice", "Bob", "   Hello   there!  How    are you?   ");
-        System.out.println("Original Message:\n" + message);
+        Message message = new Message("정훈", "윤호", "   Hey,  what  are  you   doing?   ");
 
         message.swapSenderReceiver();
-        System.out.println("\nAfter Swapping Sender and Receiver:\n" + message);
+        System.out.println(message);
 
         message.compressMessage(1);
-        System.out.println("\nAfter Compression (Option 1):\n" + message);
+        System.out.println(message);
 
         message.compressMessage(2);
-        System.out.println("\nAfter Compression (Option 2):\n" + message);
+        System.out.println(message);
 
         message.compressMessage(3);
-        System.out.println("\nAfter Compression (Option 3):\n" + message);
+        System.out.println(message);
     }
 }
