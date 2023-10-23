bf185b3f6a194212585f6b4c1eb9a936d5e36ac1
chulhwan99 <chulhwankim99@gmail.com>
2023-10-23 14:14:46 +0900

    chap5

--- SelectCourse2/CH/Chap05/AdditionQuestion.java
+++ SelectCourse2/CH/Chap05/AdditionQuestion.java
@@ -26,19 +26,54 @@ class Question implements IntQuestion {
 
 }
 
+class plusQuestion implements IntQuestion {
+    private int a, b; // The numbers in the problem.
+
+    public plusQuestion() { // constructor
+        a = (int) (Math.random() * 50 + 1);
+        b = (int) (Math.random() * 50);
+    }
+
+    public String getQuestion() {
+        return "What is " + a + " + " + b + " ?";
+    }
+
+    public int getCorrectAnswer() {
+        return a + b;
+    }
+}
+
 public class AdditionQuestion {
 
-    private static IntQuestion[] questions;
+    private static IntQuestion[] questions = new IntQuestion[10];
 
     private static int[] answer = new int[10];
 
     public static void quizManager() {
         Scanner scan = new Scanner(System.in);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 3; i++) {
             questions[i] = new Question();
             System.out.print(questions[i].getQuestion());
             answer[i] = scan.nextInt();
         }
+
+        IntQuestion bigQuestion = new IntQuestion() {
+            public String getQuestion() {
+                return "What is the answer to the ultimate question " +
+                        " of life, the universe, and everything?";
+            }
+
+            public int getCorrectAnswer() {
+                return 42;
+            }
+        };
+        questions[3] = bigQuestion;
+        System.out.println(questions[3].getQuestion());
+        answer[3] = scan.nextInt();
+
+        questions[4] = new plusQuestion();
+        System.out.println(questions[4].getQuestion());
+        answer[4] = scan.nextInt();
 
         gradeQuiz();
         scan.close();
@@ -46,7 +81,7 @@ public class AdditionQuestion {
 
     public static void gradeQuiz() {
         int count = 0;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             if (questions[i].getCorrectAnswer() == answer[i]) {
                 System.out.println(questions[i].getQuestion() + answer[i] + "정답입니다");
                 count++;
@@ -55,7 +90,7 @@ public class AdditionQuestion {
                         questions[i].getQuestion() + answer[i] + "오답입니다. 정답은 = " + questions[i].getCorrectAnswer());
             }
         }
-        System.out.println("10점 만점에 " + count + "점입니다");
+        System.out.println("5점 만점에 " + count + "점입니다");
     }
 
     public static void main(String[] args) {
--- SelectCourse2/CH/Chap05/prac05_05.java
+++ SelectCourse2/CH/Chap05/prac05_05.java
@@ -1,12 +1,89 @@
 package SelectCourse2.CH.Chap05;
 
+import java.util.Scanner;
+
 public class prac05_05 {
 
-    public boolean playGame(){
-        -
+    public static Scanner scan = new Scanner(System.in);
+
+    public boolean playGame() {
+        Deck dealerDeck = new Deck();
+        Deck playerDeck = new Deck();
+        dealerDeck.shuffle();
+        playerDeck.shuffle();
+        BlackjackHand dealerBlackjackHand = new BlackjackHand();
+        BlackjackHand playerBlackjackHand = new BlackjackHand();
+
+        dealerBlackjackHand.addCard(dealerDeck.dealCard());
+        dealerBlackjackHand.addCard(dealerDeck.dealCard());
+
+        if (dealerBlackjackHand.getBlackjackValue() == 21) {
+            System.out.println("딜러의 카드 합이 21입니다.");
+            return false;
+        }
+
+        playerBlackjackHand.addCard(playerDeck.dealCard());
+        playerBlackjackHand.addCard(playerDeck.dealCard());
+        if (playerBlackjackHand.getBlackjackValue() == 21) {
+            System.out.println("플레이어의 카드 합이 21입니다.");
+            return true;
+        }
+
+        System.out.println(
+                "현재 플레이어의 카드 합은 " + playerBlackjackHand.getBlackjackValue() + " 입니다 멈추시려면 Stand, 더 받으시려면 Hit을 입력해주세요");
+        String input = scan.nextLine();
+        while (input.equals("Hit")) {
+            playerBlackjackHand.addCard(playerDeck.dealCard());
+            if (playerBlackjackHand.getBlackjackValue() > 21) {
+                System.out.println(playerBlackjackHand.getBlackjackValue() + "로 패배하셨습니다");
+                return false;
+            }
+            System.out.println(
+                    "현재 플레이어의 카드 합은 " + playerBlackjackHand.getBlackjackValue()
+                            + " 입니다 멈추시려면 Stand, 더 받으시려면 Hit을 입력해주세요");
+            input = scan.nextLine();
+        }
+
+        while (dealerBlackjackHand.getBlackjackValue() <= 16) {
+            dealerBlackjackHand.addCard(dealerDeck.dealCard());
+            if (dealerBlackjackHand.getBlackjackValue() > 21) {
+                System.out.println("딜러가 버스트됐습니다.");
+                return true;
+            }
+        }
+
+        System.out.printf("플레이어 패의 총합은 %d, 딜러 패의 총합은 %d \n", playerBlackjackHand.getBlackjackValue(),
+                dealerBlackjackHand.getBlackjackValue());
+        if (dealerBlackjackHand.getBlackjackValue() >= playerBlackjackHand.getBlackjackValue()) {
+            return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-
+        int money = 100;
+        boolean wantContinue = true;
+        int batMoney = 0;
+        String input = "";
+        prac05_05 game = new prac05_05();
+        while (money > 0 && wantContinue) {
+            System.out.println("배팅할 금액을 입력하세요");
+            batMoney = scan.nextInt();
+            if (batMoney > money) {
+                System.out.println("보유한 금액보다 많은 금액을 입력해 최대 금액으로 설정됩니다.");
+                batMoney = money;
+            }
+            scan.nextLine();
+            if (game.playGame()) {
+                money += batMoney;
+            } else {
+                money -= batMoney;
+            }
+            System.out.println(money + "원 남았습니다. 더 하시겠습니까? yes or no");
+            input = scan.nextLine();
+            if (input.equals("no")) {
+                wantContinue = false;
+            }
+        }
     }
 }
