0ac9b5d45fa4a149377a3a023bad1a14e436ee6e
chulhwan99 <chulhwankim99@gmail.com>
2023-10-25 10:34:38 +0900

    fix : prac5_3

--- SelectCourse2/CH/Chap05/prac5_3.java
+++ SelectCourse2/CH/Chap05/prac5_3.java
@@ -1,10 +1,13 @@
 package SelectCourse2.CH.Chap05;
 
 public class prac5_3 {
+    static final int NUMBER_OF_EXPERIMENTS = 10000;
+
     public static PairOfDice p = new PairOfDice();
 
     public static int twoDiceSumSimulator(int i) {
         int count = 0;
+        p.roll();
         while (i != p.diceSum()) {
             p.roll();
             count++;
@@ -16,7 +19,7 @@ public class prac5_3 {
 
         for (int i = 2; i <= 12; i++) {
             StatCalc s = new StatCalc();
-            for (int j = 0; j < 10000; j++) {
+            for (int j = 0; j < NUMBER_OF_EXPERIMENTS; j++) {
                 s.enter(twoDiceSumSimulator(i));
             }
             System.out.println(i);
